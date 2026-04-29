08f7ba433b790c0d670074499c9056ee82886fe9
Sumit Kumar <sharansumitkumar@gmail.com>
2026-04-29 17:30:12 +0530

    code updated

---
+++ example/android/app/build.gradle.kts
@@ -0,0 +1,44 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    compileSdk = 37
+    ndkVersion = "30.0.14904198"
+    namespace = "io.flutter.plugins.connectivityexample"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_21)
+        }
+    }
+
+    defaultConfig {
+        applicationId = "io.flutter.plugins.connectivityexample"
+        minSdk = 24
+        targetSdk = 37
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+
+flutter {
+    source = "../.."
+}
+
